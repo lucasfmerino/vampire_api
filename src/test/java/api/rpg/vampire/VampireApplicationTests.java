5f9ecd0007d589d5f41921e53dcdcaf58ebf4c85
lucasfmerino <lucasfmerino@gmail.com>
2024-09-25 22:57:42 -0300

    feat: added test profile configuration

--- src/test/java/api/rpg/vampire/VampireApplicationTests.java
+++ src/test/java/api/rpg/vampire/VampireApplicationTests.java
@@ -3,8 +3,10 @@ package api.rpg.vampire;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VampireApplicationTests {
 
 	@Test
